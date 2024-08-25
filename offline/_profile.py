3493d71cef1eeeb700f07f43d9d47ff6c1adc109
JungKH33 <melon345@yonsei.ac.kr>
2024-08-25 15:56:15 +0900

    dependency fixes
    Updated requirements file: Modified the paths for local dependencies. Also included offline/third_party/Objection_Detection_Metrics/lib.

--- offline/_profile.py
+++ offline/_profile.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
                                  'emotion_classification', 'wildfire_detection', 'wildlife_recognition',
                                  'scene_recognition', 'traffic_detection'])
     parser.add_argument('--weights', type=str)
-    parser.add_argument('--host', type=str, default='192.168.137.1')
-    parser.add_argument('--port', type=int, default=9999)
+    parser.add_argument('--host', type=str, default='192.168.0.27')
+    parser.add_argument('--port', type=int, default= 9999)
     parser.add_argument('--buffer', type=int, default=1024 * 1024)
-    parser.add_argument('--data_dir', type=str, default=r'C:\Users\lxhan2\data')
+    parser.add_argument('--data_dir', type=str, default=r"F:\Projects\CPP\Pantheon-main\Pantheon_Datasets_Models\0_Datasets")
     parser.add_argument('--batch_size', type=int, default=256)
     parser.add_argument('--num_workers', type=int, default=4)
     args = parser.parse_args()
